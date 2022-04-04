d51e30903096f0edf9f0799f4baf6798d60a3967
xjapax <jvv.joaovitor559@gmail.com>
2022-04-04 00:31:46 -0300

    primeiro commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageHeader from "../template/pageHeader";
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Somos a turma +TOP</p>
+        <h2>Missão e Visão</h2>
+        <p>Aqui fazemos Sistemas Brutos</p>
+    </div>
+);
---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import IconButton from "../template/iconButton";
+
+export default props => (
+    <div role='form' className="todoForm">
+            <div className="col-xs-12 col-sm-9 col-md-10">
+                <input id='description' className='form-control'
+                    placeholder="Adicione uma tarefa"
+                    onChange={props.handleChange}
+                    value={props.description}></input>
+            </div>
+
+            <div className=" col-xs-12 col-sm-3 col-md-2">
+                <IconButton style='primary' icon='plus'
+                    onClick={props.handleAdd}></IconButton>
+            </div>
+
+    </div>
+)
